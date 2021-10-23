80410baa6f042a757df3148fbd447e1c1e24b909
SAM <samin080269@gmail.com>
2021-10-23 15:32:31 +0600

    init

---
+++ Main.js
@@ -0,0 +1,5 @@
+// const open = document.getElementById('click');
+const Skill = document.getElementsByClassName('Skill');
+document.getElementById('click').addEventListener("click" , function() {
+    Skill.classList.add('show');
+});
