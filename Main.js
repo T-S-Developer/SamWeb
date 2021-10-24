794bf3c752782451ab65a3d5755c2e1c8d1f12d8
SAMonSAM <84613821+SAMonSAM@users.noreply.github.com>
2021-10-24 14:47:07 +0600

    Delete Main.js

--- Main.js
+++
@@ -1,5 +0,0 @@
-// const open = document.getElementById('click');
-const Skill = document.getElementsByClassName('Skill');
-document.getElementById('click').addEventListener("click" , function() {
-    Skill.classList.add('show');
-});
